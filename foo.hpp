e7d4fb56df8483dd852943e643b703f68a272dc7
agatkagolec-ux <agatka.golec@gmail.com>
2025-11-25 16:13:41 +0100

    zejsciowka4

--- foo.hpp
+++ foo.hpp
@@ -7,6 +7,15 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    std::vector< char > result;
+    result.reserve(people.size());
+
+    for (Human& h : people) {
+        h.birthday();                       // inkrementacja wieku
+        char c = h.isMonster() ? 'n' : 'y'; // isMonster() ? n/y
+        result.push_back(c);
+    }
+
+    std::reverse(result.begin(), result.end()); // odwrócenie kolejnoœci
+    return result;
 }
